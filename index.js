c97f860526071941e138775d1193e91882914af3
abhishek <abhisheksathala296@gmail.com>
2025-12-13 15:08:13 +0530

    add

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 // make express server bro es6 module use impport
 
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 // fake product data get method
 app.get('/products', (req, res) => {
-  res.json([
-    { id: 1, name: 'product 1', price: 100 },
-    { id: 2, name: 'product 2', price: 200 },
-    { id: 3, name: 'product 3', price: 300 },
-  ]);
+  // res.json([
+  //   { id: 1, name: 'product 1', price: 100 },
+  //   { id: 2, name: 'product 2', price: 200 },
+  //   { id: 3, name: 'product 3', price: 300 },
+  // ]);
+
+  res.send('hello');
 });
-app.listen(PORT, () => {
-  console.log('server is running on port 3000');
-});
+
+mongoose
+  .connect('mongodb://admin:12345678@localhost:27017/admin')
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('MongoDB connection failed:', error.message);
+  });
