6872a8a08fb0e375d4a37db2dfc47421c9f4583d
abhishek <abhisheksathala296@gmail.com>
2025-12-15 17:47:48 +0530

    add_dd

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// make express server bro es6 module use impport
+// make express server bro es6 module use import
 
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
 import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT;
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 // fake product data get method
-app.get('/products', (req, res) => {
-  // res.json([
-  //   { id: 1, name: 'product 1', price: 100 },
-  //   { id: 2, name: 'product 2', price: 200 },
-  //   { id: 3, name: 'product 3', price: 300 },
-  // ]);
-  res.send('hello');
-});
+// app.get('/products', (req, res) => {
+// res.json([
+//   { id: 1, name: 'product 1', price: 100 },
+//   { id: 2, name: 'product 2', price: 200 },
+//   { id: 3, name: 'product 3', price: 300 },
+// ]);
+//   res.send('hello');
+// });
 
 mongoose
   .connect('mongodb://admin:12345678@localhost:27017/admin')
