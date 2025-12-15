3acffc010468d6f94819c79d665777d0ac375189
abhishek <abhisheksathala296@gmail.com>
2025-12-15 18:32:12 +0530

    add

---
+++ conditionals.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+number=5
+if [ number -gt 5 ]; then
+       echo "the number is not grater then 5"
+else
+    echo "the number is  grater then 5"
+fi
---
+++ variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+name = "abhishek"
+
+echo "Hello,$name!"
+
